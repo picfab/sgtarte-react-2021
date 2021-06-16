87aac56c52b6ffd3977d52d955d4d1861a61e4cb
Fabien <pic.fab@hotmail.fr>
2021-06-16 14:26:50 +0200

    feature: eslint prettier config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import motiv from './img/motiv.gif'
-export default function App(){
+
+export default function App() {
   return (
     <div className='content'>
       <h1>Ready for a new App !!!</h1>
-      <img src={motiv}/>
+      <img src={motiv} alt='trop bien' />
     </div>
   )
 }
